5c80d45db3ffdf14dfd78b12dba61e3821767082
NicholasFHayden <nicholasfhayden@gmail.com>
2016-10-03 14:50:34 -0500

    Reformat

--- project2Queries.sql
+++ project2Queries.sql
@@ -1,27 +1,31 @@
-1. SELECT first_name, last_name
-		FROM person
-		WHERE admission IN	
-			(SELECT admission
-				FROM student
-				WHERE admission= 2016);
+--Query 1
+SELECT first_name, last_name
+    FROM person
+    WHERE admission IN	
+        (SELECT admission
+         FROM student
+         WHERE admission= 2016);
 
-3. SELECT first_name, last_name
-		FROM person
-		WHERE major IN
-			(SELECT major
-				FROM student
-				WHERE major= 'MATH');
+--Query 3
+SELECT first_name, last_name
+    FROM person
+    WHERE major IN
+        (SELECT major
+         FROM student
+         WHERE major= 'MATH');
 
-4. SELECT DISTINCT state
-		FROM person
-		WHERE admission IN
-			(SELECT admission
-				FROM student
-				WHERE admission= 2016);
-				
-5. SELECT DISTINCT state
-		FROM person
-		WHERE major IN
-			(SELECT major
-				FROM student
-				WHERE major= 'MATH');
+--Query 4
+SELECT DISTINCT state
+    FROM person
+    WHERE admission IN
+        (SELECT admission
+         FROM student
+         WHERE admission= 2016);
+
+--Query 5
+SELECT DISTINCT state
+    FROM person
+    WHERE major IN
+        (SELECT major
+         FROM student
+         WHERE major= 'MATH');
